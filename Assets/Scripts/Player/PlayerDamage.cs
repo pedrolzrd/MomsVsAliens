d5d935e67fd99f98500f0a22c4d338c79b13ff98
pedrolzrd <phev2004@gmail.com>
2024-04-09 15:08:38 -0300

    Dano, Vida e seletor de personagem

---
+++ Assets/Scripts/Player/PlayerDamage.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour
+{
+    SpriteRenderer sr;
+    Rigidbody2D rb;
+    Health health;
+
+    bool canTakeDamage = true;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        health = GetComponent<Health>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy") && canTakeDamage == true)
+        {
+            health.TakeDamage(1);
+            StartCoroutine(Damage());
+        }
+    }
+
+    IEnumerator Damage()
+    {
+        canTakeDamage = false;
+        sr.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = true;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = true;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = true;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = true;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = true;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = true;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = true;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        sr.enabled = true;
+        canTakeDamage = true;
+    }
+}
