8dd63d591d43023529857bc103910b75da3d9e89
Jucielly silva <juciellysouzasilva@gmail.com>
2024-05-31 18:17:36 -0300

    oi

--- Assets/Scripts/Menu's/CharSelection.cs
+++ Assets/Scripts/Menu's/CharSelection.cs
@@ -1,27 +1,40 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CharSelection : MonoBehaviour
 {
     public GameObject[] characters;
     public int selectedChar = 0;
+    public GameObject[] charNames;
+    public GameObject PlayButton;
+
 
     public void NextChar()
     {
         characters[selectedChar].SetActive(false);
+        charNames[selectedChar].SetActive(false);
         selectedChar = (selectedChar + 1) % characters.Length;
         characters[selectedChar].SetActive(true);
+        charNames[selectedChar].SetActive(true);
+
+        PlayButton.SetActive(selectedChar == 0);
+        
+
     }
 
     public void PreviousChar()
     {
         characters[selectedChar].SetActive(false);
+        charNames[selectedChar].SetActive(false);
         selectedChar--;
         if(selectedChar < 0)
         {
             selectedChar += characters.Length;
         }
         characters[selectedChar].SetActive(true);
+        charNames[selectedChar].SetActive(true);
+        PlayButton.SetActive(selectedChar == 0);
     }
 
     public void StartGame()
